45e9972af5a1ba574d67547407f57bf1bf180648
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-12 20:12:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float funzione (float a[], int num) {
+    
+    float max = a[0];
 
+    for (int i=0; i<num; i++) {
+        if (a[i]>max) {
+            max = a[i];
+        }
+    }
+    return max;
+}

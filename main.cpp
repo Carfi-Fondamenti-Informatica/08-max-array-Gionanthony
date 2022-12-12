8d554d5bd0670dbd6e9c9f4dd012b500fc840811
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-12 20:13:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int num;
+    cin >> num;
+    float a[num];
+
+    for (int i=0; i<num; i++) {
+        cin >> a[i];
+    }
+
+    cout << funzione(a, num) ;
+
+    return 0;
 }
